d65a926f6359903bae7f824debd783168564e3fb
anmol52490 <anmol52490@gmail.com>
2026-08-25 22:56:40 +0530

    added 2d array for board creation and function for showing board

--- chess.c
+++ chess.c
@@ -17,8 +17,48 @@ void manual_board(){
     }
 }
 
+
+void show_chess_board(int chessarr[][8], int row, int col){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    for (i;i<row;i++){
+        for (j; j<col; j++){
+            printf("\t %d \t",chessarr[i][j]);
+            // k++;
+        }
+        j = 0;
+        printf("\n");
+    }
+
+}
+
+
+void create_array_board(int chessarr[][8], int row){
+    int i = 0;
+    int j = 0;
+
+    for(i;i<row;i++){
+        for (j;j<row;j++){
+            chessarr[i][j]=(i+1)*10+(j+1);
+            // printf("\t%d\n",chessarr[i][j]);
+        }
+        j= 0;
+    }
+    show_chess_board(chessarr, row, row);
+}
+
+
+
+
+
 int main(){
-    manual_board();
-    
+
+    int row = 8;
+    int col = 8;
+    int chessarr[row][col];
+
+    // manual_board();
+    create_array_board(chessarr, row);
 }
 
